b62df0b55aa819f247c9d4175dd2a6175a6abd0b
vmedrano14c <141966048+vmedrano14c@users.noreply.github.com>
2023-08-30 17:38:52 -0500

    intento 3
    Hice cambio en el codigo

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -1,38 +1,29 @@
 package classroom;
-
 public class Grupo {
-
-    public Persona[] estudiantes;
-    public Persona profesor;
-    public Asignatura asignatura;
-    public final int codigo;
-    public String horario;
-
+    private Persona[] estudiantes;
+    private Persona profesor;
+    private Asignatura asignatura;
+    private final int codigo;
+    private String horario;
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        this.estudiantes = estudiantes;
+    	this.estudiantes = estudiantes;
         this.profesor = profesor;
         this.asignatura = asignatura;
         this.codigo = codigo;
         this.horario = horario;
     }
-
     public Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        this.estudiantes = new Persona[cantidadEstudiantes];
-        this.profesor = profesor;
-        this.asignatura = asignatura;
-        this.codigo = codigo;
-        this.horario = horario;
+        Persona[]personas = new Persona[cantidadEstudiantes];
+        this(personas, profesor, asignatura, codigo, horario);
     }
-
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
-        this.estudiantes = estudiantes;
+        this.codigo = 0;
+    	this.estudiantes = estudiantes;
         this.profesor = profesor;
         this.asignatura = asignatura;
-        this.codigo = 0; 
-        this.horario = "";
     }
 
-    void cambiarEstudiante(Persona estudianteViejo, Persona estudianteNuevo) {
+    public void cambiarEstudiante(Persona estudianteViejo, Persona estudianteNuevo) {
         for (int i = 0; i < estudiantes.length; i++) {
             if (estudiantes[i].getCedula() == estudianteViejo.getCedula()) {
                 estudiantes[i] = estudianteNuevo;
@@ -41,7 +32,34 @@ public class Grupo {
         }
     }
     
-    void cambiarEstudiante(int indice, Persona estudiante) {
+    public void cambiarEstudiante(int indice, Persona estudiante) {
         estudiantes[indice] = estudiante;
+    }
+    public Persona[] getEstudiantes() {
+    	return estudiantes;
+    }
+    public void setEstudiantes(Persona[] estudiantes) {
+    	this.estudiantes = estudiantes;
+    }
+    public Persona getProfesor() {
+    	return profesor;
+    }
+    public void setProfesor(Persona profesor) {
+    	this.profesor = profesor;
+    }
+    public Asignatura getAsignatura() {
+    	return asignatura;
+    }
+    public void setAsignatura(Asignatura asignatura) {
+    	this.asignatura = asignatura;
+    }
+    public int getCodigo() {
+    	return codigo;
+    }
+    public String getHorario() {
+    	return horario;
+    }
+    public void setHorario(String horario) {
+    	this.horario = horario;
     }
 }
--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -1,42 +1,21 @@
 package classroom;
-
 public class Persona {
-
-    final long cedula;
-    String nombre;
-    static int totalPersonas;
-    
-    static {
-        totalPersonas = 0;
-    }   
-
-    public static int getTotalPersonas(){
-        return totalPersonas;
-    }
-    public Persona(long cedula, String nombre) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
-    }
-    
+	private static int totalPersonas = 0;
+    private long cedula;
+    private String nombre;
     public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
+    	this.cedula = cedula;
+    	this.nombre = nombre;
+    }
+    public Persona(long cedula) {
+    	this.cedula = cedula;
+    	this.nombre = "";
+    	totalPersonas++;
+    }
+    public Persona(String nombre) {
         this.nombre = nombre;
         totalPersonas++;
     }
-
-    public Persona(long cedula) {
-        this.cedula = cedula;
-        this.nombre = "";
-        totalPersonas++;
-    }
-
-    public Persona(String nombre) {
-        this.nombre = "";
-        this.cedula = 1;
-        totalPersonas++;
-    }
-    
     public long getCedula() {
         return cedula;
     }
